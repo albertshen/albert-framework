edc6a9a2975e80e33299421cc4be96212af6188d
Albert Shen <albertshen1206@gmail.com>
2017-11-27 15:23:33 +0800

    i

--- config/router.php
+++ config/router.php
@@ -3,8 +3,8 @@
 $routers = array();
 //System
 $routers['/wechat/callback'] = array('WechatBundle\Wechat', 'callback');
-$routers['/wechat/curio/callback'] = array('WechatBundle\Coach', 'callback');
-$routers['/wechat/curio/receive'] = array('WechatBundle\Coach', 'receiveUserInfo');
+$routers['/wechat/coach/callback'] = array('WechatBundle\Coach', 'callback');
+$routers['/wechat/coach/receive'] = array('WechatBundle\Coach', 'receiveUserInfo');
 $routers['/wechat/jssdk/config/js'] = array('WechatBundle\Wechat', 'jssdkConfigJs');
 $routers['/simulation/login'] = array('WechatBundle\Wechat', 'simulationLogin');
 $routers['/clear'] = array('CampaignBundle\Page', 'clearCookie');
